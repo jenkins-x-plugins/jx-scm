ad03f2b96c71066c26892f6ab95034d0b0e282c8
Waciuma Wanjohi <lwanjohi@pivotal.io>
2022-05-04 18:07:20 -0400

    feat: add flag to pull request create that allows update of existing pull request

--- pkg/cmd/pull_request/create/create_test.go
+++ pkg/cmd/pull_request/create/create_test.go
@@ -33,7 +33,25 @@ func TestCreatePullRequest(t *testing.T) {
 	err = o.Run()
 	assert.NoError(t, err, "failed to create the pull request")
 
-	prs, _, err := scmClient.PullRequests.List(context.TODO(), fullName, scm.PullRequestListOptions{})
+	prs, _, err := scmClient.PullRequests.List(context.TODO(), fullName, &scm.PullRequestListOptions{})
+	assert.NoError(t, err, "failed to list pull requests")
+	assert.Equal(t, 1, len(prs))
+	assert.Equal(t, prs[0].Title, o.Title, "title not properly set")
+	assert.Equal(t, prs[0].Body, o.Body, "body not properly set")
+	assert.Equal(t, prs[0].Head.Ref, o.Head, "head not properly set")
+	assert.Equal(t, prs[0].Base.Ref, o.Base, "base not properly set")
+	assert.Equal(t, 1, prs[0].Number, "unexpected pr number set")
+
+	o.Title = "An updated PR title"
+	o.Body = "New reason for change"
+	o.Head = "some_feature_branch"
+	o.Base = "main"
+	o.AllowUpdate = true
+
+	err = o.Run()
+	assert.NoError(t, err, "failed to update existing pull request")
+
+	prs, _, err = scmClient.PullRequests.List(context.TODO(), fullName, &scm.PullRequestListOptions{})
 	assert.NoError(t, err, "failed to list pull requests")
 	assert.Equal(t, 1, len(prs))
 	assert.Equal(t, prs[0].Title, o.Title, "title not properly set")
